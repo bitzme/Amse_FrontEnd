83f8a0a94c484234e1fc71b5a90f008e2e6fc487
BitzMe <Bigboom555@gmail.com>
2023-10-02 20:47:46 +0700

    Vue-Router

---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>About Page</h2>
+  <p>About...</p>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>Home</h2>
+  <p>Welcome to Home</p>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style></style>
